c4eea626f18f7b77cd7b29936f801df32be5f3d2
Tugdual Saunier <tugdual.saunier@gmail.com>
2012-11-14 11:31:47 +0100

    Fixed vnc_refresh_screen This is not his reponsability to read framebuffer (avoid infinite loop)

--- src/libvirt-php.c
+++ src/libvirt-php.c
@@ -3225,6 +3225,8 @@ PHP_FUNCTION(libvirt_domain_get_screenshot)
 	if (strcmp(name, hostname) == 0)
 		hostname = strdup("localhost");
 
+	vnc_refresh_screen(hostname, tmp, scancode);
+
 	if (use_builtin == 1) {
 		DPRINTF("%s: Binary not found, using builtin approach to %s:%s, tmp file = %s\n", PHPFUNC, hostname, tmp, file);
 
@@ -3234,7 +3236,6 @@ PHP_FUNCTION(libvirt_domain_get_screenshot)
 		}
 	}
 	else {
-		vnc_refresh_screen(hostname, tmp, scancode);
 		port = atoi(tmp)-5900;
 
 		DPRINTF("%s: Getting screenshot of %s:%d to temporary file %s\n", PHPFUNC, hostname, port, file);
